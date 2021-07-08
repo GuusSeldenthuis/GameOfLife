38cc44bbe56348c85ff419d4b50c9de077ee46b1
Guus Seldenthuis <guus1993@gmail.com>
2021-07-08 10:26:18 +0200

    Fixed pixel rendering.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 init_state = "     " + \
 			 "  #  " + \
-			 "  #  " + \
+			 " ### " + \
 			 "  #  " + \
 			 "     "
 
@@ -15,7 +15,7 @@ def make_cells(init_state):
 	i, j = 0, 0
 	new_cells = [[0 for x in range(w)] for y in range(h)]
 	for char in range(0, len(init_state)):
-		new_cells[i][j] = True if init_state[char] == " " else False
+		new_cells[i][j] = True if init_state[char] == "#" else False
 
 		# Up i,j's based on the x,y's
 		i += 1
@@ -30,33 +30,26 @@ cells = make_cells(init_state)
 for row in cells:
     print(' '.join([str(elem) for elem in row]))
 
-# cells[0][0] = True
-# cells[0][1] = True
-# cells[0][2] = True
-# cells[0][3] = True
-# cells[0][4] = True
-# cells[0][5] = True
-
-
 # DRAW PIXELS
-images = []
+frames = []
 # 10 frames to render.
 for frame in range(1):
 	# PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
-	images.append(Image.new( 'RGB', (250,250), "black")) # create a new black image
-	pixels = images[frame].load() # create the pixel map
+	frames.append(Image.new( 'RGB', (250,250), "#fff")) # create a new black image
+	pixels = frames[frame].load() # create the pixel map
 	
 	x, y = 0, 0
 	for row in cells:
 		x = 0
 		for cell in row:
-			draw_cell = [(50, 50), (x * 50, y * 50)]
+			draw_cell = [(x * 50, y * 50), ((x + 1) * 50), ((y + 1) * 50)]
 			# create rectangle image
-			img1 = ImageDraw.Draw(images[frame])
+			img1 = ImageDraw.Draw(frames[frame])
 			print("[" + str(x) + "][" + str(y) + "]: " + str(cells[x][y]))
-			img1.rectangle(draw_cell, fill = "#ffffff" if cells[x][y] else "#000000")
+			colour = "#fff" if cells[x][y] else "#000"
+			img1.rectangle(draw_cell, fill = colour)
 			x += 1
 		y += 1
 
-# img.save("image.jpg", "JPEG")
-images[0].save('out.gif', save_all=True, append_images=images[1:], optimize=True, loop=0)
+# render the gif
+frames[0].save('out.gif', save_all=True, append_images=frames[1:], optimize=True, loop=0)
